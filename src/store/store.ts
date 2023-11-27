cf465b011071ae95485f411c025835d62e84d98b
ArtemKhairov <artemxairoff@yandex.ru>
2023-11-27 21:43:59 +0700

    add export type for Store

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,4 @@ export const store = configureStore({
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-
-console.log(store.getState())
+export type RootState = ReturnType<typeof store.getState>;
